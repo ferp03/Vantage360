c5b054787614edfc7f37d07df9f78be81f6e3bf3
Fernando Perez <ferpega03@gmail.com>
2025-03-31 16:33:30 -0600

    credenciales front

--- Frontend/src/envs/environment.ts
+++ Frontend/src/envs/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:3000',
-    supabaseUrl: 'https://upextgkesjxyvmyqzgzd.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwZXh0Z2tlc2p4eXZteXF6Z3pkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5OTQ5MDksImV4cCI6MjA1NzU3MDkwOX0.enqZe9mNDWGNRKPmTUsWYqy_8VHfDy2eckGteRq_i9s'
+    supabaseUrl: 'https://mhqmohefvrdqqivdeath.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ocW1vaGVmdnJkcXFpdmRlYXRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxMTIyMDEsImV4cCI6MjA1ODY4ODIwMX0.UMnY-KrogNiafyjIHiqvNAZRxRFYmO6N8bg-uVG_90g'
 };
