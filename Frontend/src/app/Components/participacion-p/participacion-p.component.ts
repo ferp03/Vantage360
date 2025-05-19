45a1965c43c3ea32d488b30c5c2f8179a8560611
Miguel <A01383674@tec.mx>
2025-05-19 13:34:26 -0600

    Partipacion

---
+++ Frontend/src/app/Components/participacion-p/participacion-p.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-participacion-p',
+  imports: [],
+  templateUrl: './participacion-p.component.html',
+  styleUrl: './participacion-p.component.css'
+})
+export class ParticipacionPComponent {
+
+}
