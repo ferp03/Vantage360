872cdcb45e94f9e3977cced2a644a5c14adbc9ae
Fernando Perez <ferpega03@gmail.com>
2025-05-26 15:26:35 -0600

    angular utiliza backend hosteado

--- Frontend/src/envs/environment.ts
+++ Frontend/src/envs/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000',
+    // apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://vantage360-backend.vercel.app/api',
     supabaseUrl: 'https://mhqmohefvrdqqivdeath.supabase.co',
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ocW1vaGVmdnJkcXFpdmRlYXRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxMTIyMDEsImV4cCI6MjA1ODY4ODIwMX0.UMnY-KrogNiafyjIHiqvNAZRxRFYmO6N8bg-uVG_90g'
 };
